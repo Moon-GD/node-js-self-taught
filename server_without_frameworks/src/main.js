e9ad709a44eba6bf9e9e89e5205c743e700fc4ca
Moon-gd <king4mun@gmail.com>
2023-04-02 17:40:10 +0900

    백엔드 Part 1 - ⑤ : type safety checking

--- server_without_frameworks/src/main.js
+++ server_without_frameworks/src/main.js
@@ -1,11 +1,44 @@
+// @ts-check
+
 const http = require('http');
 const { SERVER_STATUS, SERVER_MESSAGE } = require('./common');
 
+/**
+ * @typedef blogDB
+ * @property {number} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {blogDB[]} */
+const blogs = [
+  {
+    id: 1,
+    title: '1번 제목',
+    content: '1번 내용',
+  },
+  {
+    id: 2,
+    title: '2번 제목',
+    content: '2번 내용',
+  },
+  {
+    id: 3,
+    title: '3번 제목',
+    content: '3번 내용',
+  },
+];
+
 const server = http.createServer((req, res) => {
+  const POST_REG_URL = /^\/posts\/([0-9])+$/;
+  const postTokens = req.url && POST_REG_URL.exec(req.url);
+
   if (req.url === '/posts' && req.method === 'GET') {
     res.statusCode = SERVER_STATUS.SUCCESS;
     res.end(SERVER_MESSAGE.ALL_POST);
-  } else if (/^\/posts\/[0-9]+$/.test(req.url) && req.method === 'GET') {
+  } else if (req.url && POST_REG_URL.test(req.url) && req.method === 'GET') {
+    const postID = postTokens && postTokens[1];
+    console.log('postID : ', postID);
     res.statusCode = SERVER_STATUS.SUCCESS;
     res.end(SERVER_MESSAGE.SOME_POST);
   } else if (req.url === '/posts' && req.method === 'POST') {
